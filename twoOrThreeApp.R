5946cfe83b8714db94cb8467622d7a9cad3fb05a
dbalaji2000 <balajid@umich.edu>
2021-04-21 15:41:53 -0400

    Update twoOrThreeApp.R

--- twoOrThreeApp.R
+++ twoOrThreeApp.R
@@ -59,59 +59,37 @@ ui <- fluidPage(
   )
 )
 
-score <- function(shot_input, player){
-  
-  shot2 <- c(2, 0)
-  shot3 <- c(3, 0)
-  
-  kd2_prob <- c(data$twoptmake[which(data$X1==player)], data$twoptmiss[which(data$X1==player)])
-  kd3_prob <- c(data$threeptmake[which(data$X1==player)], data$threeptmiss[which(data$X1==player)])
-  
-  
-  kd2samp <- sample(shot2, size = shot_input, replace = TRUE, prob = kd2_prob)
-  kd3samp <- sample(shot3, size = shot_input, replace = TRUE, prob = kd3_prob)
-  
-  kd2cumulative <- cumsum(kd2samp)
-  kd3cumulative <- cumsum(kd3samp)
-  
-  # kd2cumulative
-  # kd3cumulative
-  
-  shot_attempt <- seq(1, number_of_shots, by = 1)
-  
-  kd_df <- data.frame(shot_attempt, kd2cumulative, kd3cumulative)
-  #kd_df
-}  
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
  shot_input <- reactive(input$shots)
  player <- reactive(input$player)
- 
- # score <- function(shot_input, player){
- #   
- #   shot2 <- c(2, 0)
- #   shot3 <- c(3, 0)
- #   
- #   kd2_prob <- c(data$twoptmake[which(data$X1==player)], data$twoptmiss[which(data$X1==player)])
- #   kd3_prob <- c(data$threeptmake[which(data$X1==player)], data$threeptmiss[which(data$X1==player)])
- #   
- #   
- #   kd2samp <- sample(shot2, size = shot_input, replace = TRUE, prob = kd2_prob)
- #   kd3samp <- sample(shot3, size = shot_input, replace = TRUE, prob = kd3_prob)
- #   
- #   kd2cumulative <- cumsum(kd2samp)
- #   kd3cumulative <- cumsum(kd3samp)
- #   
- #   # kd2cumulative
- #   # kd3cumulative
- #   
- #   shot_attempt <- seq(1, number_of_shots, by = 1)
- #   
- #   kd_df <- data.frame(shot_attempt, kd2cumulative, kd3cumulative)
- #   #kd_df
- # }  
+
+ score <- function(shot_input, player){
+
+   shot2 <- c(2, 0)
+   shot3 <- c(3, 0)
+
+   kd2_prob <- c(data$twoptmake[which(data$X1==player)], data$twoptmiss[which(data$X1==player)])
+   kd3_prob <- c(data$threeptmake[which(data$X1==player)], data$threeptmiss[which(data$X1==player)])
+
+
+   kd2samp <- sample(shot2, size = shot_input, replace = TRUE, prob = kd2_prob)
+   kd3samp <- sample(shot3, size = shot_input, replace = TRUE, prob = kd3_prob)
+
+   kd2cumulative <- cumsum(kd2samp)
+   kd3cumulative <- cumsum(kd3samp)
+
+   # kd2cumulative
+   # kd3cumulative
+
+   shot_attempt <- seq(1, number_of_shots, by = 1)
+
+   kd_df <- data.frame(shot_attempt, kd2cumulative, kd3cumulative)
+   #kd_df
+ }
+}
    output$plot <- renderPlot({
        ggplot()+
        geom_line(data = kd_df, aes(x = kd_df$shot_attempt,
@@ -128,8 +106,6 @@ server <- function(input, output) {
    })
 
     
-    
-}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
